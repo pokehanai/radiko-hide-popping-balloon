71adcc27761e0fdff3c72fecb615698264740f6f
HANAI tohru <tohru@reedom.com>
2018-04-23 10:34:49 +0900

    style: js

--- handler.js
+++ handler.js
@@ -1,8 +1,8 @@
 (function () {
-  var playerDetailObserver = function(mutationsList) {
-    let tooltip = document.querySelector('#player-detail .tooltip');
+  const playerDetailObserver = function (mutationsList) {
+    const tooltip = document.querySelector('#player-detail .tooltip');
     if (tooltip) tooltip.remove();
   };
-  var observer = new MutationObserver(playerDetailObserver);
-  observer.observe(document.querySelector('#player-detail'), { childList: true});
+  const observer = new MutationObserver(playerDetailObserver);
+  observer.observe(document.querySelector('#player-detail'), { childList: true });
 })();
